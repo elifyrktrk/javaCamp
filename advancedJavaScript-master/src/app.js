2a270d67e3402b2773f6140e353161b1d7fcdabd
elifyrktrk <elifyrktrk@gmail.com>
2021-06-05 22:01:07 +0300

    day 10

---
+++ advancedJavaScript-master/src/app.js
@@ -0,0 +1,40 @@
+//var,let,const
+let sayi1 = 10;
+sayi1 = "Engin Demiroğ"
+let student = {id:1, name:"Engin"}
+//console.log(student);
+ 
+function save(puan=10,ogrenci) {
+    console.log(ogrenci.name + ":" + puan)
+}
+//save(undefined,student);
+
+let students = ["Engin Demiroğ","Halit Kalaycı","Engin Toprak","Büşra"]
+//console.log(students)
+
+let students2 = [student,{id:1, name:"Engin"},"Ankara",{city:"İstanbul"}]
+//console.log(students2)
+
+//rest  
+//params
+//varArgs
+let showProducts = function (id,...products) {
+    console.log(id)
+    console.log(products[0])
+}
+//console.log(typeof showProducts)
+//showProducts(10,["elma","armut","karpuz"])
+
+//spread
+let points = [1,2,3,4,50,4,60,14]
+console.log(...points)
+console.log(Math.max(...points))
+console.log(..."ABC","D",..."EFG","H")
+
+//Destructuring
+let populations=[10000,20000,30000]
+let [small,medium,high] = populations
+
+console.log(small)
+console.log(medium)
+console.log(high)
