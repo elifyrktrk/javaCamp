dc99828e7edd2148936db0051bed28027f7ac0bc
elifyrktrk <elifyrktrk@gmail.com>
2021-07-11 21:13:43 +0300

    day 8

--- northwind/src/main/java/kodlama/io/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlama/io/northwind/business/abstracts/ProductService.java
@@ -2,6 +2,7 @@ package kodlama.io.northwind.business.abstracts;
 
 import java.util.List;
 
+
 import kodlama.io.northwind.core.utilities.result.DataResult;
 import kodlama.io.northwind.core.utilities.result.Result;
 import kodlama.io.northwind.entities.concretes.Product;
@@ -11,6 +12,19 @@ public interface ProductService {
 	Result add(Product product);
 
 
-    DataResult<Product> getByProductName(String productName);
+	 DataResult<Product>  getByProductName(String productName);
+	 
+	 DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);
+	 
+	 DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId);
+
+	 DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+	 
+	 DataResult<List<Product>> getByProductNameContains(String productName);
+	 
+	 DataResult<List<Product>> getByProductNameStartsWith(String productName);
+
+	 DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
+
       
 }
