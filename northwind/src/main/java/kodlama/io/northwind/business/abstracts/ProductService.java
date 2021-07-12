5c9ba31b44c0a978d1fb870a48c531437f209406
elifyrktrk <elifyrktrk@gmail.com>
2021-07-12 11:09:20 +0300

    day 8

--- northwind/src/main/java/kodlama/io/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlama/io/northwind/business/abstracts/ProductService.java
@@ -9,8 +9,9 @@ import kodlama.io.northwind.entities.concretes.Product;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
+	DataResult<List<Product>> getAll(int pageNo, int pageSize);
+	DataResult<List<Product>> getAllSorted();
 	Result add(Product product);
-
 
 	 DataResult<Product>  getByProductName(String productName);
 	 
--- northwind/src/main/java/kodlama/io/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlama/io/northwind/business/concretes/ProductManager.java
@@ -3,6 +3,9 @@ package kodlama.io.northwind.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.northwind.business.abstracts.ProductService;
@@ -78,5 +81,19 @@ public class ProductManager implements ProductService{
 	public DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId) {
 		return new SuccessDataResult<List<Product>>
 		(this.productDao.getByNameAndCategory(productName, categoryId),"Data listelendi"); 
+	}
+
+	@Override
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+		
+		Pageable pageable = PageRequest.of(pageNo-1, pageSize);
+		
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(pageable).getContent());
+	}
+
+	@Override
+	public DataResult<List<Product>> getAllSorted() {
+		Sort sort = Sort.by(Sort.Direction.DESC, "productName");
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(sort),"Başarılı");
 	}
 }
